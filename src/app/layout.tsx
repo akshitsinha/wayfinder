85c49fdcc8b2da112ff7ad0d74b9fe2f5628b979
Akshit Sinha <akshitsinha@outlook.com>
2025-03-12 16:13:15 +0530

    chore: update deps

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import ServiceWorkerRegistrar from "@/components/ServiceWorker";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -21,7 +20,6 @@ export default function RootLayout({
       </head>
       <body>
         <main>{children}</main>
-        <ServiceWorkerRegistrar />
       </body>
     </html>
   );
